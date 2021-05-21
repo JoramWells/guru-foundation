a938703a2eef184d135fba4bf8eec1c9449def2a
unknown <jorammanoah1@gmail.com>
2021-05-20 19:23:21 -0700

    Added useContext

---
+++ src/components/contexts/AuthContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../../auth/firebase";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  function signup() {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
--- src/components/login/Signin.js
+++ src/components/login/Signin.js
@@ -5,9 +5,9 @@ export default function Signin() {
     const emailRef = useRef()
     const passwordRef = useRef()
   return (
-    <Row>
-      <Card>
-        <Form>
+    <Row justify="center" style={{margin:"1rem"}}>
+      <Card style={{width:"25rem"}}>
+        <Form layout="vertical">
           <Form.Item label="email" ref={emailRef} name="email">
             <Input  />
           </Form.Item>
