3c069e7aefcf246cae18678db2ae216dfb76fc26
unknown <jorammanoah1@gmail.com>
2021-05-20 18:54:27 -0700

    Added dependacies

--- src/components/login/Signin.js
+++ src/components/login/Signin.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Row, Form, Card, Button } from "antd";
 
 export default function Signin() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Row>
+      <Card>
+        <Form>
+          <Form.Item label="email" ref={emailRef} name="email">
+            <Input value={value} />
+          </Form.Item>
+          <Form.Item label="Password" ref={passwordRef} name="password">
+            <Input.Password value={value} />
+          </Form.Item>
+          <Form.Item>
+              <Button>Submit</Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </Row>
+  );
 }
